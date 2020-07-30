0297166e565de21d503c8428f7776e73d7c068ba
Admin <mihailbichkov@yandex.ru>
2020-07-31 02:16:19 +0500

    4. candicates.jsp - список кандидатов. [#309007]

--- src/main/java/com/dream/store/Store.java
+++ src/main/java/com/dream/store/Store.java
@@ -1,6 +1,7 @@
 package com.dream.store;
 
 import com.dream.model.Post;
+import com.dream.model.Candidate;
 
 import java.util.Collection;
 import java.util.Map;
@@ -12,17 +13,26 @@ public class Store {
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
         posts.put(2, new Post(2, "Middle Java Job"));
         posts.put(3, new Post(3, "Senior Java Job"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
