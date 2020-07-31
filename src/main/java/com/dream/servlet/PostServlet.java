0bd5d19049a92141154495046587187497b6070a
Admin <mihailbichkov@yandex.ru>
2020-07-31 15:48:50 +0500

    1. Servlet. Web.xml [#309017]

---
+++ src/main/java/com/dream/servlet/PostServlet.java
@@ -0,0 +1,20 @@
+package com.dream.servlet;
+
+import com.dream.model.Post;
+import com.dream.store.Store;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PostServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doPost(req, resp);
+        req.setCharacterEncoding("UTF-8");
+        Store.instOf().save(new Post(0, req.getParameter("name")));
+        resp.sendRedirect(req.getContextPath() + "/posts.jsp");
+    }
+}
